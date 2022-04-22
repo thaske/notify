801d95ad4ef88d3a86458d2533c2c2dd9fbc5bc9
thaske <10328778+thaske@users.noreply.github.com>
2022-04-21 21:14:14 -0400

    Remove node-notifier

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 import axios from "axios";
 import cron from "node-cron";
 import * as cheerio from "cheerio";
-import notifier from "node-notifier";
 import { exec } from "child_process";
 
 const { URL, SELECTOR, INSTOCK } = process.env;
 
-async function main() {
+cron.schedule("*/15 * * * * *", async () => {
   const { data } = await axios.get(URL);
   const $ = cheerio.load(data);
   const status = $(SELECTOR).first().text();
-
-  process.stdout.cursorTo(0);
-  process.stdout.write(`${new Date().toLocaleTimeString()}\t${status}`);
-
+  
+  console.log(`${new Date().toLocaleTimeString()}\t${status}`);
+  
   if (status === INSTOCK) {
     exec(`open -a "Google Chrome" ${URL}`);
-    notifier.notify("In stock");
     process.exit(0);
   }
-}
-
-cron.schedule("*/15 * * * * *", main);
+});
