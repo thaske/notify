d1af720feab43f48114b9130905b22b8cc8e7678
thaske <10328778+thaske@users.noreply.github.com>
2022-04-21 21:20:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ cron.schedule("*/15 * * * * *", async () => {
   const { data } = await axios.get(URL);
   const $ = cheerio.load(data);
   const status = $(SELECTOR).first().text();
-  
   console.log(`${new Date().toLocaleTimeString()}\t${status}`);
   
   if (status === INSTOCK) {
