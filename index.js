61c54e911ed8ab34769b8c3a5d4fcad79add8855
thaske <10328778+thaske@users.noreply.github.com>
2022-12-25 19:24:02 -0500

    Clean up printing name

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ import * as cheerio from "cheerio";
 import { exec } from "child_process";
 
 const { SECONDS } = process.env;
-const sites = jsonc.parse(fs.readFileSync("sites.jsonc", "utf8"));
+const SITES = jsonc.parse(fs.readFileSync("sites.jsonc", "utf8"));
 
 cron.schedule(`*/${SECONDS ?? 15} * * * * *`, async () => {
-  const promises = Object.values(sites).map(
-    async ({ URL, SELECTOR, INSTOCK }) => {
+  const promises = Object.entries(SITES).map(
+    async ([site, { URL, SELECTOR, INSTOCK }]) => {
       try {
         const { data } = await axios.get(URL);
         const $ = cheerio.load(data);
@@ -20,17 +20,15 @@ cron.schedule(`*/${SECONDS ?? 15} * * * * *`, async () => {
           status = "OUT OF STOCK";
         }
 
-        console.log(`${new Date().toLocaleTimeString()}\t${status}\t${URL}`);
+        console.log(`${new Date().toLocaleTimeString()}\t${status}\t${site}`);
 
         if (status === INSTOCK) {
           exec(`open -a "Google Chrome" ${URL}`);
           process.exit(0);
         }
       } catch (error) {
-        console.error(error, URL);
+        console.error;
       }
     }
   );
-
-  await Promise.all(promises);
 });
